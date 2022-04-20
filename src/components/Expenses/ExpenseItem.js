15344a5f6c5a1b697946bca6e18244ea9a756887
João Vitor Wilke Silva <wilke.joao@gmail.com>
2022-04-19 21:42:36 -0700

    Re-arranged the folder structure to make it cleaner and organized

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,5 @@
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
     const priceFormatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });    
