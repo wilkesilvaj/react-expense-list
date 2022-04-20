f0cf5fa757cdc40c20c864e4a81b1fe10214504b
João Vitor Wilke Silva <wilke.joao@gmail.com>
2022-04-19 21:07:12 -0700

    Moved ExpenseDate onto its own component and customized it

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
-    const priceFormatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
+    const priceFormatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });    
   return (
     <div key={props.id} id={props.id} className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{priceFormatter.format(props.amount)}</div>
