abcc2c7a02267897dafd2574b7d667bb06f9300e
João Vitor Wilke Silva <wilke.joao@gmail.com>
2022-04-20 19:07:02 -0700

    Modified ExpenseItem to make use of useState to modify the title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,24 @@
+import React, {useState} from 'react';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title); // React hook
     const priceFormatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });    
+    const clickHandler = () => {
+      let newTitle = title + '!';
+      setTitle(newTitle);
+      // setTitle('Teeeeeest');
+    }
+
   return (
     <Card key={props.id} id={props.id} className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{priceFormatter.format(props.amount)}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
